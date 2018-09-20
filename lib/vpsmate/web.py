f12ba77f3ecf3afceabed9d716b495c6ac6d093e
myklss <myclsky@gmail.com>
2018-09-21 00:34:51 +0800

    cs

--- lib/vpsmate/web.py
+++ lib/vpsmate/web.py
@@ -2717,12 +2717,10 @@ class BackendHandler(RequestHandler):
             repofile = '/etc/yum.repos.d/centalt.repo'
             if os.path.exists(repofile):
                 lines = []
-               # baseurl_found = False
-				baseurl_found = True
+                baseurl_found = True
                 with open(repofile) as f:
                     for line in f:
-                     #   if line.startswith('baseurl='):
-						if line.startswith('baseurl=http://mirrors.neu.edu.cn/CentALT/$releasever/$basearch'):
+                        if line.startswith('baseurl=http://mirrors.neu.edu.cn/CentALT/$releasever/$basearch'):
                             baseurl_found = True
                             line = '#%s' % line
                             lines.append(line)
